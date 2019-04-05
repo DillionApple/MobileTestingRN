48feccdb9f74fe093da30ec1096db1be24e7d7dc
DillionApple <dillionmango@gmail.com>
2019-04-05 20:49:56 +0800

    add map tests

--- App.js
+++ App.js
@@ -12,14 +12,32 @@ import HomeScreen from "./src/screens/HomeScreen"
 import NetworkTestScreen from "./src/screens/NetworkTestScreen"
 import UITestScreen from "./src/screens/UITestScreen"
 import MapTestScreen from "./src/screens/MapTestScreen"
+import AnimatedMarkers from "./src/screens/MapTestCases/AnimatedMarkers";
 
 import {createStackNavigator, createAppContainer} from "react-navigation"
+import AnimatedNavigation from "./src/screens/MapTestCases/AnimatedNavigation";
+import DisplayLatLng from "./src/screens/MapTestCases/DisplayLatLng";
+import ViewsAsMarkers from "./src/screens/MapTestCases/ViewsAsMarkers";
+import EventListener from "./src/screens/MapTestCases/EventListener";
+import MarkerTypes from "./src/screens/MapTestCases/MarkerTypes";
+import DraggableMarkers from "./src/screens/MapTestCases/DraggableMarkers";
+import PolygonCreator from "./src/screens/MapTestCases/PolygonCreator";
+import DefaultMarkers from "./src/screens/MapTestCases/DefaultMarkers";
 
 const MainNavigator = createStackNavigator({
   Home: {screen: HomeScreen},
   NetworkTest: {screen: NetworkTestScreen},
   UITest: {screen: UITestScreen},
   MapTest: {screen: MapTestScreen},
+  MapTest_AnimatedMarkers: {screen: AnimatedMarkers},
+  MapTest_AnimatedNavigation: {screen: AnimatedNavigation},
+  MapTest_DisplayLatLng: {screen: DisplayLatLng},
+  MapTest_ViewsAsMarkers: {screen: ViewsAsMarkers},
+  MapTest_EventListener: {screen: EventListener},
+  MapTest_MarkerTypes: {screen: MarkerTypes},
+  MapTest_DraggableMarkers: {screen: DraggableMarkers},
+  MapTest_PolygonCreator: {screen: PolygonCreator},
+  MapTest_DefaultMarkers: {screen: DefaultMarkers},
 }, {
   initialRouteName: 'Home',
   defaultNavigationOptions: {
---
+++ src/screens/MapTestCases/AnimatedNavigation.js
@@ -0,0 +1,122 @@
+import React, { Component } from 'react';
+
+import {
+    View,
+    StyleSheet,
+    TouchableOpacity,
+    Text,
+    Button
+} from 'react-native';
+
+import MapView from 'react-native-maps';
+import carImage from './assets/car.png';
+
+export default class NavigationMap extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            prevPos: null,
+            curPos: { latitude: 37.420814, longitude: -122.081949 },
+            curAng: 45,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+        };
+        this.changePosition = this.changePosition.bind(this);
+        this.getRotation = this.getRotation.bind(this);
+        this.updateMap = this.updateMap.bind(this);
+    }
+
+    changePosition(latOffset, lonOffset) {
+        const latitude = this.state.curPos.latitude + latOffset;
+        const longitude = this.state.curPos.longitude + lonOffset;
+        this.setState({ prevPos: this.state.curPos, curPos: { latitude, longitude } });
+        this.updateMap();
+    }
+
+    getRotation(prevPos, curPos) {
+        if (!prevPos) return 0;
+        const xDiff = curPos.latitude - prevPos.latitude;
+        const yDiff = curPos.longitude - prevPos.longitude;
+        return (Math.atan2(yDiff, xDiff) * 180.0) / Math.PI;
+    }
+
+    updateMap() {
+        const { curPos, prevPos, curAng } = this.state;
+        const curRot = this.getRotation(prevPos, curPos);
+        this.map.animateCamera({ heading: curRot, center: curPos, pitch: curAng });
+    }
+
+    render() {
+        return (
+            <View style={styles.flex}>
+                <MapView
+                    ref={(el) => (this.map = el)}
+                    style={styles.flex}
+                    minZoomLevel={15}
+                    initialRegion={{
+                        ...this.state.curPos,
+                        latitudeDelta: this.state.latitudeDelta,
+                        longitudeDelta: this.state.longitudeDelta,
+                    }}
+                >
+                    <MapView.Marker
+                        coordinate={this.state.curPos}
+                        anchor={{ x: 0.5, y: 0.5 }}
+                        image={carImage}
+                    />
+                </MapView>
+                <View style={styles.buttonContainer}>
+                    <TouchableOpacity
+                        style={[styles.button]}
+                        onPress={() => this.changePosition(0.0001, 0)}
+                    >
+                        <Text>+ Lat</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={[styles.button]}
+                        onPress={() => this.changePosition(-0.0001, 0)}
+                    >
+                        <Text>- Lat</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={[styles.button]}
+                        onPress={() => this.changePosition(0, -0.0001)}
+                    >
+                        <Text>- Lon</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={[styles.button]}
+                        onPress={() => this.changePosition(0, 0.0001)}
+                    >
+                        <Text>+ Lon</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    flex: {
+        ...StyleSheet.absoluteFillObject,
+        flexDirection: 'column',
+        justifyContent: 'flex-end',
+        alignItems: 'stretch',
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        marginVertical: 30,
+        backgroundColor: 'transparent',
+        justifyContent: 'space-between',
+    },
+    button: {
+        height: 50,
+        borderRadius: 5,
+        paddingHorizontal: 8,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginHorizontal: 5,
+        backgroundColor: 'rgba(255, 255, 255, 0.7)'
+    },
+});
