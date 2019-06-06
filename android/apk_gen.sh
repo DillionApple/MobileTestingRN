99b55b07b34d43cb5798ddbdefee75200a597780
DillionApple <dillionmango@gmail.com>
2019-06-06 14:24:11 +0800

    generate signed apk from command line

---
+++ android/apk_gen.sh
@@ -0,0 +1,14 @@
+ZIP_ALIGN=/Users/dillion/Library/Android/sdk/build-tools/28.0.3/zipalign
+APK_SIGNER=/Users/dillion/Library/Android/sdk/build-tools/28.0.3/apksigner
+UNSIGNED_APK_PATH=./app/build/outputs/apk/release/app-release-unsigned.apk
+ALIGNED_APK_PATH=./app/build/outputs/apk/release/app-release-aligned.apk
+SIGNED_APK_PATH=./app/build/outputs/apk/release/app-release-signed.apk
+APK_DST_PATH=~/Desktop/mobile_testing.apk
+
+# ./gradlew assembleDebug
+
+$ZIP_ALIGN -v -p 4 $UNSIGNED_APK_PATH $ALIGNED_APK_PATH
+
+$APK_SIGNER sign --ks release-key.jks --out $SIGNED_APK_PATH $ALIGNED_APK_PATH
+
+cp $SIGNED_APK_PATH $APK_DST_PATH
